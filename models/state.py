d855d35e9414c52e2c077bf2b5048a1551f0de5f
ethanpasta <ethan.mayer9@gmail.com>
2019-08-13 23:37:17 +0000

    Add new sqlalchemy properties

--- models/state.py
+++ models/state.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import models
+from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
-    """This is the class for State
+class State(BaseModel, Base):
+    """ This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute for FileStorage environment """
+            return [city for city in models.storage.all(City) if
+                    city.state_id == self.id]

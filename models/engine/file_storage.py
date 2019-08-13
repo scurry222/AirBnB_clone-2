a9d1143680e8d19dd17fac342770771be4a699d2
scurry222 <scurry222@gmail.com>
2019-08-13 21:57:24 +0000

    implementation of delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,14 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if cls:
+            return {key: obj for (key, obj) in self.__objects.items()
+                    if isinstance(obj, cls)}
         return self.__objects
 
     def new(self, obj):
@@ -55,6 +58,14 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes an object from __objects if obj exists
+        """
+        if obj in self.__objects.values():
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del(self.__objects[key])
+        return
 
     def classes(self):
         """Returns a dictionary of valid classes and their references."""

101dcccc64b8b2a9e4d6888ef1c63fc4f69b4b75
ethanpasta <ethan.mayer9@gmail.com>
2019-08-13 23:30:17 +0000

    Remove key '_sa_instance_state' in to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -44,7 +44,7 @@ class BaseModel:
             returns a string of class name, id, and dictionary
         """
         return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
+            type(self).__name__, self.id, self.to_dict())
 
     def __repr__(self):
         """return a string representaion
@@ -64,6 +64,8 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
+        if '_sa_instance_state' in my_dict:
+            del my_dict['_sa_instance_state']
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()

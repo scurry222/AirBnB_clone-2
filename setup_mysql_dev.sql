41237e0e9033bf48457777fd4401dd3973195509
scurry222 <scurry222@gmail.com>
2019-08-13 23:37:29 +0000

    adding user functionality

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- prepares a MySQL server for the AirBnB clone
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';

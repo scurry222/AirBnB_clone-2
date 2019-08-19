0f93a75860715fb1988858cfd33432b9ec41624b
scurry222 <scurry222@gmail.com>
2019-08-19 20:58:59 +0000

    fixing import error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """ Fabric script that generates a .tgz archive from the contents of the
     web_static folder of the AirBnB Clone repo
 """
-from fabric.api import *
+from fabric.operations import local
 import datetime
 
 
@@ -11,7 +11,7 @@ def do_pack():
     """
     time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    command = local("tar czvf versions/web_static_{}.tgz web_static"
+    command = local("tar -czvf versions/web_static_{}.tgz web_static"
                     .format(time))
     if command.failed:
         return None

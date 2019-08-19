c40301c29fd83c9b22f9ca0a39303f65acb61ceb
scurry222 <scurry222@gmail.com>
2019-08-19 20:56:48 +0000

    pep8 compliant

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,6 +10,7 @@ import re
 
 env.hosts = ['104.196.55.234', '35.229.108.9']
 
+
 @runs_once
 def do_pack():
     """ archives web static
@@ -44,32 +45,33 @@ def do_deploy(archive_path):
     if result.failed:
         return False
     result = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-              .format(filename, filename))
+                 .format(filename, filename))
     if result.failed:
         return False
     result = run("rm /tmp/{}.tgz".format(filename))
     if result.failed:
         return False
     result = run("mv /data/web_static/releases/{}"
-              "/web_static/* /data/web_static/releases/{}/"
-              .format(filename, filename))
+                 "/web_static/* /data/web_static/releases/{}/"
+                 .format(filename, filename))
     if result.failed:
         return False
     result = run("rm -rf /data/web_static/releases/{}/web_static"
-              .format(filename))
+                 .format(filename))
     if result.failed:
         return False
     result = run("rm -rf /data/web_static/current")
     if result.failed:
         return False
     result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-              .format(filename))
+                 .format(filename))
     if result.failed:
         return False
 
     sudo("sudo service nginx restart")
 
     return True
+
 
 def deploy():
     """packs and deploys web static
